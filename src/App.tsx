781563c3240cd5ebee547e2df7173e12fe416b7f
Tru Dang <trudang2409@gmail.com>
2025-08-22 10:53:02 +0700

    fit bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import Body from "./components/Body";
-import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <div>
-        {/* <Header/> */}
         <Body/>
       </div>
 
